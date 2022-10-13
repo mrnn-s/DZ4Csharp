e280a85d94e7fe41453731ce59f44c97f12c06a1
MariannaSarkisian <spn.mjj@gmail.com>
2022-10-13 22:23:48 +0300

    4z

--- 4z/Program.cs
+++ 4z/Program.cs
@@ -1,1 +1,32 @@
 ﻿// В одномерном массиве из 123 чисел найти количество элементов из отрезка [10,99]
+void FillArray(int[] array, int min, int max)
+{
+    int length = array.Length;
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = new Random().Next (min,max);
+    }
+}
+void PrintArray(int[]array)
+{
+    int length = array.Length;
+    for (int i = 0; i < length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+int[] array = new int[123];
+FillArray(array, -999, 999);
+PrintArray(array);
+int number = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if  (array[i]>=10 && array[i]<=99)                                 
+        {
+            int count = 1;
+            number = number+ count;
+        }
+    
+}
+Console.WriteLine();
+Console.Write($"В массиве {number} чисел  из диапазона от 10 до 99");
