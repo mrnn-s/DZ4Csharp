1308466a41b03ee0f9cf9b17abc5c9b00a50e286
MariannaSarkisian <spn.mjj@gmail.com>
2022-10-13 22:56:04 +0300

    5z

--- 5z/Program.cs
+++ 5z/Program.cs
@@ -1,1 +1,40 @@
 ﻿// Найти сумму чисел одномерного массива стоящих на нечетной позиции
+void FillArray(int[] array, int min, int max)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+      Random N = new Random();
+      array [i] = N.Next(min, max);
+    }
+}
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+  {
+    Console.Write($"{array[i]} ");
+  }
+}
+
+Console.WriteLine("enter length array");
+int n = int.Parse(Console.ReadLine() ?? "0");
+int[] array = new int[n];
+
+Console.WriteLine("enter  min, max");
+Console.WriteLine("enter min");
+int min = int.Parse(Console.ReadLine() ?? "0");
+
+Console.WriteLine("enter max");
+int max = int.Parse(Console.ReadLine() ?? "0");
+FillArray( array, min, max);
+PrintArray(array);
+
+int sum = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (i%2==1)
+    {
+       sum = sum + array[i];
+    }
+}
+Console.WriteLine();
+Console.WriteLine($"Сумма чисел, стоящих на нечетных позициях, равна  {sum}");
--- 6z/Program.cs
+++ 6z/Program.cs
@@ -1,2 +1,33 @@
 ﻿//Найти произведение пар чисел в одномерном массиве. 
 //Парой считаем первый и последний элемент, второй и предпоследний и т.д.
+void FillArray(int[] array, int min, int max)
+{
+    int length = array.Length;
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = new Random().Next (min,max);
+    }
+}
+void PrintArray(int[]array)
+{
+    int length = array.Length;
+    for (int i = 0; i < length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+int[] array = new int[123];
+FillArray(array, -999, 999);
+PrintArray(array);
+int number = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if  (array[i]>=10 && array[i]<=99)                                 
+        {
+            int count = 1;
+            number = number+ count;
+        }
+    
+}
+Console.WriteLine();
+Console.Write($"В массиве {number} чисел  из диапазона от 10 до 99");
